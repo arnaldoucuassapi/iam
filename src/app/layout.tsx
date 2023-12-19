51ba51613f2dd77fd8c6ae4e553981f615a4f892
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-12-19 16:36:34 +0000

    hello world

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Manrope } from 'next/font/google'
+import './globals.css'
+
+const manrope = Manrope({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'arnaldoucuassapi',
+  description: 'Hey, I’m Arnaldo Ucuassapi 👋',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={manrope.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hey, I’m Arnaldo Ucuassapi 👋</h1>
+    </main>
+  )
+}
