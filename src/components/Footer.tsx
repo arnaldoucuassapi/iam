02d2bf0ad5e462af1263199d7439341c932ef51b
arnaldoucuassapi <arnaldoucuassapi@gmail.com>
2024-02-24 20:14:17 +0000

    feat: improve footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,25 @@
 
 export function Footer() {
   return (
-    <footer className="bg-zinc-900 text-center py-4 border-t border-zinc-800">
-      <p className="text-white">Created by me &copy; Todos os direitos reservados · 2024</p>
+    <footer className="bg-zinc-900/50 py-8 px-16 mt-16 shadow-xl">
+      <p 
+        className="text-xl font-semibold mb-4 bg-gradient-to-r from-blue-500 to-white text-transparent bg-clip-text"
+      >
+        Nice to see you here!
+      </p>
+
+      <div className="text-zinc-300 text-sm">
+        <p>arnaldoucuassapi@gmail.com</p>
+        <p>This website is best viewed on desktop</p>
+        <div className="flex items-center justify-between">
+          <p>Made with satisfaction © 2024 Arnaldo Ucuassapi</p>
+          <p className="flex items-center space-x-6">
+            <a href="mailto:arnaldoucuassapi@gmail.com" target="_blank">Email↗</a>
+            <a href="https://github.com/arnaldoucuassapi/" target="_blank">Github↗</a>
+            <a href="https://www.linkedin.com/in/arnaldo-ucuassapi-740252247" target="_blank">LinkedIn↗</a>
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
